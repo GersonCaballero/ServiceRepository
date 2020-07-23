530cd041b051847e0a5cfab04f122ad6a782f4fa
GersonCaballero <51335862+GersonCaballero@users.noreply.github.com>
2020-07-22 23:58:29 -0600

    CreateListTypeService
    CreateListTypeService

---
+++ Consultarte/5.6.0/angular/src/app/TypeServices/list-typeservice/list-typeservice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-typeservice',
+  templateUrl: './list-typeservice.component.html',
+  styleUrls: ['./list-typeservice.component.css']
+})
+export class ListTypeserviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
